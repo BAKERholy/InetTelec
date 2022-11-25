0ebc3c5694c9be472a06077960b2b1ccc06a25e3
unknown <BAKER_holy@mail.ru>
2022-11-25 15:50:55 +0300

    accordion

--- index.js
+++ index.js
@@ -9,3 +9,20 @@ navToggle.addEventListener("click", function (evt) {
   evt.preventDefault();
   nav.classList.toggle("navigation--closed");
 });
+
+
+document.querySelectorAll('.accordion-item__trigger').forEach((item) =>
+  item.addEventListener('click', () => {
+    const parent = item.parentNode;
+
+    if (parent.classList.contains('accordion-item--active')) {
+      parent.classList.remove('accordion-item--active');
+    } else {
+      document
+        .querySelectorAll('.accordion-item')
+        .forEach((child) => child.classList.remove('accordion-item--active'))
+
+      parent.classList.add('accordion-item--active');
+    }
+  })
+)
